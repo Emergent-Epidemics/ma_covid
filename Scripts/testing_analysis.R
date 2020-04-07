fd4dd284c62900915f4e793e8afc4e2a97aa6bad
scarpino <scarpino@utexas.edu>
2020-04-07 10:09:17 -0400

    updating data

--- Scripts/testing_analysis.R
+++ Scripts/testing_analysis.R
@@ -15,7 +15,7 @@
 ######
 #Data#
 ######
-tests <- c(1296, 1751, 2271, 3132, 4091, 5207, 6004, 8922, 13749, 19794, 23621, 29371, 35049, 39066, 42793, 46935, 51738, 56608, 62962) #taken from PDFs
+tests <- c(1296, 1751, 2271, 3132, 4091, 5207, 6004, 8922, 13749, 19794, 23621, 29371, 35049, 39066, 42793, 46935, 51738, 56608, 62962, 68800,71937,76429) #taken from PDFs
 
 source("growth_rates.R")
 
@@ -29,6 +29,6 @@ plot(as.POSIXct(strptime(diff_mat[start:stop,1], format = "%Y-%m-%d")) , rowSums
 
 plot(as.POSIXct(strptime(diff_mat[start:stop,1], format = "%Y-%m-%d")) , diff(tests), type = "b", pch = 16, lwd = 2, bty = "n", xlab = "", ylab = "Number of new COVID19 tests", main = "COVID-19 daily tests in MA")
 
-plot(as.POSIXct(strptime(diff_mat[start:stop,1], format = "%Y-%m-%d")) , rowSums(diff_mat[,-1], na.rm = T)[start:stop]/diff(tests), type = "b", ylim = c(0, 0.25), pch = 16, lwd = 2, bty = "n", xlab = "", ylab = "Proportion of tests positive", main = "COVID-19 positive test prop. in MA")
+plot(as.POSIXct(strptime(diff_mat[start:stop,1], format = "%Y-%m-%d")) , rowSums(diff_mat[,-1], na.rm = T)[start:stop]/diff(tests), type = "b", ylim = c(0, 0.3), pch = 16, lwd = 2, bty = "n", xlab = "", ylab = "Proportion of tests positive", main = "COVID-19 positive test prop. in MA")
 
 abline(lm(as.numeric( rowSums(diff_mat[,-1], na.rm = T)[start:stop]/diff(tests))~as.POSIXct(strptime(diff_mat[start:stop,1], format = "%Y-%m-%d"))), lty = 3, lwd = 2, col = "red")
